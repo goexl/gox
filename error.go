a3e445613e0ee1d227c28bf20996a9fc7d897e1f
storezhang <storezhang@gmail.com>
2020-06-23 11:15:34 +0800

    修复Error

--- error.go
+++ error.go
@@ -5,53 +5,53 @@ import (
 )
 
 type (
+	// ToErrorCode 错误码
+	ErrorCode int
+
 	// Error 接口，符合条件的错误统一处理
 	Error interface {
-		// ErrorCode 返回错误码
-		ErrorCode() int
-		// Message 返回错误消息
-		Message() string
-		// Data 返回错误实体
+		// ToErrorCode 返回错误码
+		ToErrorCode() ErrorCode
+		// ToMsg 返回错误消息
+		ToMsg() string
+		// ToData 返回错误实体
 		// 在某些错误下，可能需要返回额外的信息给前端处理
 		// 比如，认证错误，需要返回哪些字段有错误
-		Data() interface{}
+		ToData() interface{}
 	}
-
-	// ErrorCode 错误码
-	ErrorCode int
 
 	// CodeError 带错误编号和消息的错误
 	CodeError struct {
 		// 错误码
-		errorCode ErrorCode
+		ErrorCode ErrorCode
 		// 消息
-		message string
+		Msg string
 		// 数据
-		data interface{}
+		Data interface{}
 	}
 )
 
 // NewCodeError 创建错误
-func NewCodeError(errorCode ErrorCode, message string, data interface{}) *CodeError {
-	return &CodeError{
-		errorCode: errorCode,
-		message:   message,
-		data:      data,
+func NewCodeError(errorCode ErrorCode, msg string, data interface{}) CodeError {
+	return CodeError{
+		ErrorCode: errorCode,
+		Msg:       msg,
+		Data:      data,
 	}
 }
 
-func (ce *CodeError) ErrorCode() int {
-	return int(ce.errorCode)
+func (ce *CodeError) ToErrorCode() ErrorCode {
+	return ce.ErrorCode
 }
 
-func (ce *CodeError) Message() string {
-	return ce.message
+func (ce *CodeError) ToMsg() string {
+	return ce.Msg
 }
 
-func (ce *CodeError) Data() interface{} {
-	return ce.data
+func (ce *CodeError) ToData() interface{} {
+	return ce.Data
 }
 
 func (ce *CodeError) Error() string {
-	return fmt.Sprintf("{ErrorCode=%d, message=%s, data=%v}", ce.errorCode, ce.message, ce.data)
+	return fmt.Sprintf("{ToErrorCode=%d, Msg=%s, Data=%v}", ce.ErrorCode, ce.Msg, ce.Data)
 }
