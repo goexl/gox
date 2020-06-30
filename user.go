207dfe222c2dc95906de742fd6f9674f7b9eb9f5
tw-chen <chentw@class100.com>
2020-06-30 15:16:39 +0800

    修改DBString

--- user.go
+++ user.go
@@ -5,10 +5,10 @@ type BaseUser struct {
 	BaseStruct `xorm:"extends"`
 
 	// 用户名
-	Username DBString `xorm:"varchar(32) notnull default('') unique(uidx_name)" json:"username" validate:"omitempty,min=1,max=32,email"`
+	Username string `xorm:"varchar(32) notnull default('') unique(uidx_name)" json:"username" validate:"omitempty,min=1,max=32,email"`
 	// 手机号
-	// 类似于8617089792784
-	Phone DBString `xorm:"varchar(15) notnull default('') unique(uidx_phone)" json:"phone" validate:"omitempty,alphanum,max=15"`
+	// 类似于+86-17089792784
+	Phone string `xorm:"varchar(15) notnull default('') unique(uidx_phone)" json:"phone" validate:"omitempty,mobile"`
 	// 密码
 	Pwd string `xorm:"varchar(512) notnull default('')" json:"-"`
 }
