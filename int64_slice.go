6bd15fa5ce9d51f06531f2631eb4dd1625ad581e
tw-chen <chentw@class100.com>
2020-07-13 11:12:31 +0800

    int64slice支持从参数中获取

--- int64_slice.go
+++ int64_slice.go
@@ -21,6 +21,11 @@ func (s *Int64Slice) UnmarshalJSON(bytes []byte) error {
 	return s.unmarshalJSON(bytes)
 }
 
+func (s *Int64Slice) UnmarshalParam(src string) error {
+	values := strings.Split(src, ",")
+	return s.toInt64Slice(values)
+}
+
 func (s *Int64Slice) FromDB(bytes []byte) error {
 	return s.unmarshalJSON(bytes)
 }
@@ -41,21 +46,27 @@ func (s Int64Slice) marshalJSON(toDb bool) ([]byte, error) {
 
 func (s *Int64Slice) unmarshalJSON(bytes []byte) (err error) {
 	var values []int64
-	if err = json.Unmarshal(bytes, &values); err == nil {
+	if err = json.Unmarshal(bytes, &values); nil == err {
 		*s = values
 		return
 	}
 
 	var sValues []string
-	if err = json.Unmarshal(bytes, &sValues); err == nil {
-		*s = make([]int64, len(values))
-		for i, value := range sValues {
-			value, err := strconv.ParseInt(value, 10, 64)
-			if nil != err {
-				return err
-			}
-			(*s)[i] = value
+	if err = json.Unmarshal(bytes, &sValues); nil != err {
+		return
+	}
+
+	return s.toInt64Slice(sValues)
+}
+
+func (s *Int64Slice) toInt64Slice(values []string) (err error) {
+	iValue := int64(0)
+	*s = make([]int64, len(values))
+	for i, value := range values {
+		if iValue, err = strconv.ParseInt(value, 10, 64); nil != err {
+			return
 		}
+		(*s)[i] = iValue
 	}
 
 	return
