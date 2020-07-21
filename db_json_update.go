9eb6593efea8e1753a63568bd64f1afece1d6e36
storezhang <storezhang@gmail.com>
2020-07-21 16:30:47 +0800

    修复JSON Update

--- db_json_update.go
+++ db_json_update.go
@@ -5,26 +5,46 @@ import (
 	`fmt`
 )
 
-func DBJsonUpdate(updateTableName, updateFiledName, conditionFiledName string, condition interface{}, conf map[string]interface{}, prefix string, style SeparatorStyle) (error, string) {
+// MySQLJsonUpdateWithConfig 生成MySQL JSON增量更新SQL语句
+func MySQLJsonUpdateWithConfig(
+	table string, filed string,
+	conditionFiled string, conditionValue interface{},
+	data map[string]interface{},
+	prefix string, style SeparatorStyle,
+) (sql string, err error) {
+	var keyMap map[string]interface{}
 
 	// 检查配置是否为空
 	// 如果为空，不再继续处理，会报SQL语句错误
-	if 0 == len(conf) {
-		return errors.New("cofing is nil"), ""
+	if 0 == len(data) {
+		err = errors.New("配置数据为空")
+		return
 	}
-	sqlStr := fmt.Sprintf(`UPDATE %v SET %v = JSON_SET(%v`, updateTableName, updateFiledName)
-	if keyMap, err := Flatten(conf, prefix, style); nil != err {
-		return err, ""
-	} else {
-		for k, v := range keyMap {
-			switch v.(type) {
-			case string:
-				sqlStr += fmt.Sprintf(`,'$.%v','%v'`, k, v)
-			default:
-				sqlStr += fmt.Sprintf(`,'$.%v',%v`, k, v)
-			}
+
+	sql = fmt.Sprintf(`UPDATE %v SET %v = JSON_SET(%v`, table, filed, filed)
+	if keyMap, err = Flatten(data, prefix, style); nil != err {
+		return
+	}
+
+	for k, v := range keyMap {
+		switch v.(type) {
+		case string:
+			sql += fmt.Sprintf(`,'$.%v','%v'`, k, v)
+		default:
+			sql += fmt.Sprintf(`,'$.%v',%v`, k, v)
 		}
 	}
-	sqlStr += fmt.Sprintf(`) WHERE %v = '%v'`, conditionFiledName, condition)
-	return nil, sqlStr
+
+	sql += fmt.Sprintf(`) WHERE %v = '%v'`, conditionFiled, conditionValue)
+
+	return
+}
+
+// MySQLJsonUpdate 生成MySQL JSON增量更新SQL语句的快捷方式
+func MySQLJsonUpdate(
+	table string, filed string,
+	conditionFiled string, conditionValue interface{},
+	data map[string]interface{},
+) (sql string, err error) {
+	return MySQLJsonUpdateWithConfig(table, filed, conditionFiled, conditionValue, data, "", DotStyle)
 }
