b9c324482086de6474931841d686028aac222a9c
storezhang <storezhang@gmail.com>
2020-10-09 11:35:28 +0800

    YUNKE-1608 【Admin-Server】需要admin服务提供获取机构URL的接口

--- http.go
+++ http.go
@@ -2,4 +2,19 @@ package gox
 
 const (
 	HeaderContentType = "Content-Type"
+
+	// Http方法集合
+	// GET方法
+	HttpMethodGet HttpMethod = "get"
+	// POST方法
+	HttpMethodPost HttpMethod = "post"
+	// PUT方法
+	HttpMethodPut HttpMethod = "put"
+	// DELETE方法
+	HttpMethodDelete HttpMethod = "delete"
+)
+
+type (
+	// HttpMethod Http方法
+	HttpMethod string
 )
