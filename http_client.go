467a9ef013139df076cd873a23b32f5e307d1d44
storezhang <storezhang@gmail.com>
2021-04-06 19:02:41 +0800

    YUNKE-3283 【Server】作为云视课堂运营者，我希望所有学校能在一套系统中运行，而不是每个学校一套部署

--- http_client.go
+++ http_client.go
@@ -6,25 +6,23 @@ import (
 )
 
 // HttpClient Http客户端配置
-type (
-	HttpClient struct {
-		// Timeout 超时
-		Timeout time.Duration `json:"timeout" yaml:"timeout"`
-		// Proxy 代理
-		Proxy Proxy `json:"proxy" yaml:"proxy" validate:"structonly"`
-		// Auth 授权配置
-		Auth Auth `json:"auth" yaml:"auth" validate:"structonly"`
-		// AllowGetPayload 是否允许Get方法传输数据
-		AllowGetPayload bool `default:"true" json:"allowGetPayload" yaml:"allowGetPayload"`
-		// Certificate 秘钥配置
-		Certificate Certificate `json:"certificate" yaml:"certificate" validate:"structonly"`
-		// QueryParams 通用的查询参数
-		QueryParams map[string]string `json:"queryParams" yaml:"queryParams"`
-		// FormData 表单参数，只对POST和PUT方法有效
-		FormData map[string]string `json:"formData" yaml:"formData"`
-		// Headers 通用头信息
-		Headers map[string]string `json:"headers" yaml:"headers"`
-		// Cookies 通用Cookie
-		Cookies []*http.Cookie `json:"cookies" yaml:"cookies"`
-	}
-)
+type HttpClient struct {
+	// Timeout 超时
+	Timeout time.Duration `json:"timeout" yaml:"timeout"`
+	// Proxy 代理
+	Proxy Proxy `json:"proxy" yaml:"proxy" validate:"structonly"`
+	// Auth 授权配置
+	Auth Auth `json:"auth" yaml:"auth" validate:"structonly"`
+	// AllowGetPayload 是否允许Get方法传输数据
+	AllowGetPayload bool `default:"true" json:"allowGetPayload" yaml:"allowGetPayload"`
+	// Certificate 秘钥配置
+	Certificate Certificate `json:"certificate" yaml:"certificate" validate:"structonly"`
+	// QueryParams 通用的查询参数
+	QueryParams map[string]string `json:"queryParams" yaml:"queryParams"`
+	// FormData 表单参数，只对POST和PUT方法有效
+	FormData map[string]string `json:"formData" yaml:"formData"`
+	// Headers 通用头信息
+	Headers map[string]string `json:"headers" yaml:"headers"`
+	// Cookies 通用Cookie
+	Cookies []*http.Cookie `json:"cookies" yaml:"cookies"`
+}
