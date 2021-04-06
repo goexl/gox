34749aa70c4eb0b6d208f9f6e4ed148dcff73eac
storezhang <storezhang@gmail.com>
2021-04-06 16:07:07 +0800

    YUNKE-3283 【Server】作为云视课堂运营者，我希望所有学校能在一套系统中运行，而不是每个学校一套部署

---
+++ auth.go
@@ -0,0 +1,25 @@
+package gox
+
+const (
+	// AuthTypeBasic 基本Http授权验证
+	AuthTypeBasic AuthType = "basic"
+	// AuthTypeToken 基本传Token的授权验证
+	AuthTypeToken AuthType = "token"
+)
+
+type (
+	// AuthType 授权类型
+	AuthType string
+
+	// Auth 授权信息
+	Auth struct {
+		// Type 授权类型
+		Type AuthType `default:"type" json:"type" yaml:"type" validate:"required,oneof=basic token"`
+		// Username 用户名
+		Username string `json:"username" yaml:"username"`
+		// Password 密码
+		Password string `json:"password" yaml:"password"`
+		// Token 授权码
+		Token string `json:"token" yaml:"token"`
+	}
+)
--- http_client.go
+++ http_client.go
@@ -1,6 +1,7 @@
 package gox
 
 import (
+	`net/http`
 	`time`
 )
 
@@ -11,5 +12,17 @@ type (
 		Timeout time.Duration `json:"timeout" yaml:"timeout"`
 		// Proxy 代理
 		Proxy Proxy `json:"proxy" yaml:"proxy"`
+		// Auth 授权配置
+		Auth Auth `json:"auth" yaml:"auth"`
+		// AllowGetPayload 是否允许Get方法传输数据
+		AllowGetPayload bool `default:"true" json:"allowGetPayload" yaml:"allowGetPayload"`
+		// QueryParams 通用的查询参数
+		QueryParams map[string]string `json:"queryParams" yaml:"queryParams"`
+		// FormData 表单参数，只对POST和PUT方法有效
+		FormData map[string]string `json:"formData" yaml:"formData"`
+		// Headers 通用头信息
+		Headers map[string]string `json:"headers" yaml:"headers"`
+		// Cookies 通用Cookie
+		Cookies []http.Cookie `json:"cookies" yaml:"cookies"`
 	}
 )
