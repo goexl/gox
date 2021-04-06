2a69a16ca290d5cb4d767acdc20aad14f484e347
storezhang <storezhang@gmail.com>
2021-04-06 19:01:58 +0800

    YUNKE-3283 【Server】作为云视课堂运营者，我希望所有学校能在一套系统中运行，而不是每个学校一套部署

---
+++ certificate.go
@@ -0,0 +1,21 @@
+package gox
+
+type (
+	// ClientCertificate 客户端秘钥
+	ClientCertificate struct {
+		// Public 公钥文件路径
+		Public string `json:"public" yaml:"public" validate:"required,file"`
+		// Private 私钥文件路径
+		Private string `json:"private" yaml:"private" validate:"required,file"`
+	}
+
+	// Certificate 秘钥
+	Certificate struct {
+		// Skip 是否跳过TLS检查
+		Skip bool `default:"true" json:"skip" yaml:"skip"`
+		// Root 根秘钥文件路径
+		Root string `json:"root" yaml:"root" validate:"required,file"`
+		// Client 客户端
+		Client ClientCertificate `json:"client" yaml:"client" validate:"structonly"`
+	}
+)
--- http_client.go
+++ http_client.go
@@ -16,6 +16,8 @@ type (
 		Auth Auth `json:"auth" yaml:"auth" validate:"structonly"`
 		// AllowGetPayload 是否允许Get方法传输数据
 		AllowGetPayload bool `default:"true" json:"allowGetPayload" yaml:"allowGetPayload"`
+		// Certificate 秘钥配置
+		Certificate Certificate `json:"certificate" yaml:"certificate" validate:"structonly"`
 		// QueryParams 通用的查询参数
 		QueryParams map[string]string `json:"queryParams" yaml:"queryParams"`
 		// FormData 表单参数，只对POST和PUT方法有效
