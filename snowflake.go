71b719b1de137d5bacdc9c074ca76bd330ca470b
storezhang <storezhang@gmail.com>
2021-04-07 10:24:28 +0800

    YUNKE-3283 【Server】作为云视课堂运营者，我希望所有学校能在一套系统中运行，而不是每个学校一套部署

--- snowflake.go
+++ snowflake.go
@@ -11,122 +11,83 @@ import (
 )
 
 var (
-	// Epoch is set to the twitter snowflake epoch of Nov 04 2010 01:42:54 UTC in milliseconds
-	// You may customize this to set a different epoch for your application.
-	Epoch int64 = 1288834974657
+	// epoch 设置为2010年11月4日UTC，Twitter雪花算法开始时间（以毫秒为单位）
+	// 可以对其进行自定义，以为您的应用程序设置另一个开始时间
+	epoch int64 = 1288834974657
 
-	// NodeBits holds the number of bits to use for Snowflake
-	// Remember, you have a total 22 bits to share between Snowflake/Step
-	NodeBits uint8 = 10
+	// nodeBits 表示节点数量所占用的位数
+	// 10位的话，就表示总共有2^10（1024个节点数量）个节点数可使用
+	nodeBits uint8 = 10
 
-	// StepBits holds the number of bits to use for Step
-	// Remember, you have a total 22 bits to share between Snowflake/Step
-	StepBits uint8 = 12
+	// stepBits 表示生成编号时，可以自增的位数（大小）
+	// 12位的话，就表示总共有2^12（4096个自增数量）个自增数可使用
+	stepBits uint8 = 12
 
 	mu        sync.Mutex
-	nodeMax   int64 = -1 ^ (-1 << NodeBits)
-	nodeMask        = nodeMax << StepBits
-	stepMask  int64 = -1 ^ (-1 << StepBits)
-	timeShift       = NodeBits + StepBits
-	nodeShift       = StepBits
+	nodeMax   int64 = -1 ^ (-1 << nodeBits)
+	nodeMask        = nodeMax << stepBits
+	stepMask  int64 = -1 ^ (-1 << stepBits)
+	timeShift       = nodeBits + stepBits
+	nodeShift       = stepBits
 )
 
-const encodeBase32Map = "ybndrfg8ejkmcpqxot1uwisza345h769"
+type (
+	// Id 表示雪花算法生成出来的编号
+	Id int64
 
-var decodeBase32Map [256]byte
+	// NodeId 表示一个节点的编号
+	NodeId int64
 
-const encodeBase58Map = "123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ"
+	// Snowflake 雪花生成器
+	Snowflake struct {
+		mu    sync.Mutex
+		epoch time.Time
+		time  int64
+		node  int64
+		step  int64
 
-var decodeBase58Map [256]byte
-
-// A JSONSyntaxError is returned from UnmarshalJSON if an invalid Id is provided.
-type JSONSyntaxError struct{ original []byte }
-
-func (j JSONSyntaxError) Error() string {
-	return fmt.Sprintf("invalid snowflake Id %q", string(j.original))
-}
-
-// ErrInvalidBase58 is returned by ParseBase58 when given an invalid []byte
-var ErrInvalidBase58 = errors.New("invalid base58")
-
-// ErrInvalidBase32 is returned by ParseBase32 when given an invalid []byte
-var ErrInvalidBase32 = errors.New("invalid base32")
-
-// Create maps for decoding Base58/Base32. This speeds up the process tremendously.
-func init() {
-	for i := 0; i < len(encodeBase58Map); i++ {
-		decodeBase58Map[i] = 0xFF
+		nodeMax   int64
+		nodeMask  int64
+		stepMask  int64
+		timeShift uint8
+		nodeShift uint8
 	}
+)
 
-	for i := 0; i < len(encodeBase58Map); i++ {
-		decodeBase58Map[encodeBase58Map[i]] = byte(i)
-	}
-
-	for i := 0; i < len(encodeBase32Map); i++ {
-		decodeBase32Map[i] = 0xFF
-	}
-
-	for i := 0; i < len(encodeBase32Map); i++ {
-		decodeBase32Map[encodeBase32Map[i]] = byte(i)
-	}
-}
-
-// A Snowflake struct holds the basic information needed for a snowflake generator node
-type Snowflake struct {
-	mu    sync.Mutex
-	epoch time.Time
-	time  int64
-	node  int64
-	step  int64
-
-	nodeMax   int64
-	nodeMask  int64
-	stepMask  int64
-	timeShift uint8
-	nodeShift uint8
-}
-
-// An Id is a custom type used for a snowflake Id.  This is used so we can attach methods onto the Id.
-type Id int64
-
-// NewSnowflake returns a new snowflake node that can be used to generate snowflake IDs
-func NewSnowflake(node int64) (*Snowflake, error) {
+// NewSnowflake 创建一个雪花生成器
+func NewSnowflake(node NodeId) (snowflake *Snowflake, err error) {
 	mu.Lock()
-	nodeMax = -1 ^ (-1 << NodeBits)
-	nodeMask = nodeMax << StepBits
-	stepMask = -1 ^ (-1 << StepBits)
-	timeShift = NodeBits + StepBits
-	nodeShift = StepBits
+	nodeMax = -1 ^ (-1 << nodeBits)
+	nodeMask = nodeMax << stepBits
+	stepMask = -1 ^ (-1 << stepBits)
+	timeShift = nodeBits + stepBits
+	nodeShift = stepBits
 	mu.Unlock()
 
-	s := Snowflake{}
-	s.node = node
-	s.nodeMax = -1 ^ (-1 << NodeBits)
-	s.nodeMask = s.nodeMax << StepBits
-	s.stepMask = -1 ^ (-1 << StepBits)
-	s.timeShift = NodeBits + StepBits
-	s.nodeShift = StepBits
+	snowflake = &Snowflake{}
+	snowflake.node = int64(node)
+	snowflake.nodeMax = -1 ^ (-1 << nodeBits)
+	snowflake.nodeMask = snowflake.nodeMax << stepBits
+	snowflake.stepMask = -1 ^ (-1 << stepBits)
+	snowflake.timeShift = nodeBits + stepBits
+	snowflake.nodeShift = stepBits
 
-	if s.node < 0 || s.node > s.nodeMax {
-		return nil, errors.New("Snowflake number must be between 0 and " + strconv.FormatInt(s.nodeMax, 10))
+	if snowflake.node < 0 || snowflake.node > snowflake.nodeMax {
+		err = errors.New(fmt.Sprintf("Node必须在0和%d之间", snowflake.nodeMax))
 	}
 
 	var curTime = time.Now()
-	// add time.Duration to curTime to make sure we use the monotonic clock if available
-	s.epoch = curTime.Add(time.Unix(Epoch/1000, (Epoch%1000)*1000000).Sub(curTime))
+	// 确保单一时钟可用（减少雪花算法里面因为时间回调问题而引发的编号冲突）
+	snowflake.epoch = curTime.Add(time.Unix(epoch/1000, (epoch%1000)*1000000).Sub(curTime))
 
-	return &s, nil
+	return
 }
 
-// Next creates and returns a unique snowflake Id
-// To help guarantee uniqueness
-// - Make sure your system is keeping accurate system time
-// - Make sure you never have multiple nodes running with the same node Id
-func (s *Snowflake) Next() Id {
+// Next 返回下一个编号
+func (s *Snowflake) Next() (id Id) {
 	s.mu.Lock()
 
 	now := time.Since(s.epoch).Nanoseconds() / 1000000
-
 	if now == s.time {
 		s.step = (s.step + 1) & s.stepMask
 
@@ -138,17 +99,12 @@ func (s *Snowflake) Next() Id {
 	} else {
 		s.step = 0
 	}
-
 	s.time = now
-
-	r := Id((now)<<s.timeShift |
-		(s.node << s.nodeShift) |
-		(s.step),
-	)
+	id = Id((now)<<s.timeShift | (s.node << s.nodeShift) | (s.step))
 
 	s.mu.Unlock()
 
-	return r
+	return
 }
 
 // NextId 下一个长整形Id
@@ -161,202 +117,115 @@ func (s *Snowflake) NextString() string {
 	return s.Next().String()
 }
 
-// Int64 returns an int64 of the snowflake Id
-func (f Id) Int64() int64 {
-	return int64(f)
+// Int64 返回整形形式
+func (i Id) Int64() int64 {
+	return int64(i)
 }
 
-// ParseInt64 converts an int64 into a snowflake Id
+// ParseInt64 从整形获得编号
 func ParseInt64(id int64) Id {
 	return Id(id)
 }
 
-// String returns a string of the snowflake Id
-func (f Id) String() string {
-	return strconv.FormatInt(int64(f), 10)
+// String 返回编号的字符串形式
+func (i Id) String() string {
+	return strconv.FormatInt(int64(i), 10)
 }
 
-// ParseString converts a string into a snowflake Id
-func ParseString(id string) (Id, error) {
-	i, err := strconv.ParseInt(id, 10, 64)
+// ParseString 从十进制字符串解析编号
+func ParseString(str string) (id Id, err error) {
+	var original int64
 
-	return Id(i), err
-
-}
-
-// Base2 returns a string base2 of the snowflake Id
-func (f Id) Base2() string {
-	return strconv.FormatInt(int64(f), 2)
-}
-
-// ParseBase2 converts a Base2 string into a snowflake Id
-func ParseBase2(id string) (Id, error) {
-	i, err := strconv.ParseInt(id, 2, 64)
-
-	return Id(i), err
-}
-
-// Base32 uses the z-base-32 character set but encodes and decodes similar to base58, allowing it to create an even smaller result string.
-// NOTE: There are many different base32 implementations so becareful when doing any interoperation.
-func (f Id) Base32() string {
-	if f < 32 {
-		return string(encodeBase32Map[f])
+	if original, err = strconv.ParseInt(str, 10, 64); nil != err {
+		return
 	}
 
-	b := make([]byte, 0, 12)
-	for f >= 32 {
-		b = append(b, encodeBase32Map[f%32])
-		f /= 32
-	}
-	b = append(b, encodeBase32Map[f])
+	id = Id(original)
 
-	for x, y := 0, len(b)-1; x < y; x, y = x+1, y-1 {
-		b[x], b[y] = b[y], b[x]
-	}
-
-	return string(b)
+	return
 }
 
-// ParseBase32 parses a base32 []byte into a snowflake Id
-// NOTE: There are many different base32 implementations so becareful when doing any interoperation.
-func ParseBase32(b []byte) (Id, error) {
-	var id int64
+// Base64 返回编号的Base64字符串
+func (i Id) Base64() string {
+	return base64.StdEncoding.EncodeToString(i.Bytes())
+}
 
-	for i := range b {
-		if decodeBase32Map[b[i]] == 0xFF {
-			return -1, ErrInvalidBase32
-		}
-		id = id*32 + int64(decodeBase32Map[b[i]])
+// ParseBase64 从Base64字符串中解析编号
+func ParseBase64(str string) (id Id, err error) {
+	var bytes []byte
+
+	if bytes, err = base64.StdEncoding.DecodeString(str); nil != err {
+		return
 	}
 
-	return Id(id), nil
+	id, err = ParseBytes(bytes)
+
+	return
 }
 
-// Base36 returns a base36 string of the snowflake Id
-func (f Id) Base36() string {
-	return strconv.FormatInt(int64(f), 36)
+// Bytes 返回二进制数组表示形式
+func (i Id) Bytes() []byte {
+	return []byte(i.String())
 }
 
-// ParseBase36 converts a Base36 string into a snowflake Id
-func ParseBase36(id string) (Id, error) {
-	i, err := strconv.ParseInt(id, 36, 64)
+// ParseBytes 从二进制数组里面获得编号
+func ParseBytes(bytes []byte) (id Id, err error) {
+	var original int64
 
-	return Id(i), err
-}
-
-// Base58 returns a base58 string of the snowflake Id
-func (f Id) Base58() string {
-	if f < 58 {
-		return string(encodeBase58Map[f])
+	if original, err = strconv.ParseInt(string(bytes), 10, 64); nil != err {
+		return
 	}
 
-	b := make([]byte, 0, 11)
-	for f >= 58 {
-		b = append(b, encodeBase58Map[f%58])
-		f /= 58
-	}
-	b = append(b, encodeBase58Map[f])
+	id = Id(original)
 
-	for x, y := 0, len(b)-1; x < y; x, y = x+1, y-1 {
-		b[x], b[y] = b[y], b[x]
-	}
-
-	return string(b)
+	return
 }
 
-// ParseBase58 parses a base58 []byte into a snowflake Id
-func ParseBase58(b []byte) (Id, error) {
-	var id int64
-
-	for i := range b {
-		if decodeBase58Map[b[i]] == 0xFF {
-			return -1, ErrInvalidBase58
-		}
-		id = id*58 + int64(decodeBase58Map[b[i]])
-	}
-
-	return Id(id), nil
-}
-
-// Base64 returns a base64 string of the snowflake Id
-func (f Id) Base64() string {
-	return base64.StdEncoding.EncodeToString(f.Bytes())
-}
-
-// ParseBase64 converts a base64 string into a snowflake Id
-func ParseBase64(id string) (Id, error) {
-	b, err := base64.StdEncoding.DecodeString(id)
-	if err != nil {
-		return -1, err
-	}
-
-	return ParseBytes(b)
-
-}
-
-// Bytes returns a byte slice of the snowflake Id
-func (f Id) Bytes() []byte {
-	return []byte(f.String())
-}
-
-// ParseBytes converts a byte slice into a snowflake Id
-func ParseBytes(id []byte) (Id, error) {
-	i, err := strconv.ParseInt(string(id), 10, 64)
-
-	return Id(i), err
-}
-
-// IntBytes returns an array of bytes of the snowflake Id, encoded as a big endian integer.
-func (f Id) IntBytes() [8]byte {
+// IntBytes 返回8位二进制表示形式
+func (i Id) IntBytes() [8]byte {
 	var b [8]byte
-	binary.BigEndian.PutUint64(b[:], uint64(f))
+	binary.BigEndian.PutUint64(b[:], uint64(i))
 
 	return b
 }
 
-// ParseIntBytes converts an array of bytes encoded as big endian integer as
-// a snowflake Id
+// ParseIntBytes 从二进制数组获得编号
 func ParseIntBytes(id [8]byte) Id {
 	return Id(int64(binary.BigEndian.Uint64(id[:])))
 }
 
-// Time returns an int64 unix timestamp in milliseconds of the snowflake Id time
-func (f Id) Time() int64 {
-	return (int64(f) >> timeShift) + Epoch
+// Time 返回时间
+func (i Id) Time() int64 {
+	return (int64(i) >> timeShift) + epoch
 }
 
-// Snowflake returns an int64 of the snowflake Id node number
-func (f Id) Node() int64 {
-	return int64(f) & nodeMask >> nodeShift
+// Snowflake 返回节点数
+func (i Id) Node() int64 {
+	return int64(i) & nodeMask >> nodeShift
 }
 
-// Step returns an int64 of the snowflake step (or sequence) number
-func (f Id) Step() int64 {
-	return int64(f) & stepMask
+// Step 返回当前自增数量
+func (i Id) Step() int64 {
+	return int64(i) & stepMask
 }
 
-// MarshalJSON returns a json byte array string of the snowflake Id.
-func (f Id) MarshalJSON() ([]byte, error) {
+func (i Id) MarshalJSON() ([]byte, error) {
 	buff := make([]byte, 0, 22)
 	buff = append(buff, '"')
-	buff = strconv.AppendInt(buff, int64(f), 10)
+	buff = strconv.AppendInt(buff, int64(i), 10)
 	buff = append(buff, '"')
 
 	return buff, nil
 }
 
-// UnmarshalJSON converts a json byte array of a snowflake Id into an Id type.
-func (f *Id) UnmarshalJSON(b []byte) error {
-	if len(b) < 3 || b[0] != '"' || b[len(b)-1] != '"' {
-		return JSONSyntaxError{b}
+func (i *Id) UnmarshalJSON(data []byte) (err error) {
+	var original int64
+
+	if original, err = strconv.ParseInt(string(data[1:len(data)-1]), 10, 64); nil != err {
+		return
 	}
 
-	i, err := strconv.ParseInt(string(b[1:len(b)-1]), 10, 64)
-	if err != nil {
-		return err
-	}
+	*i = Id(original)
 
-	*f = Id(i)
-
-	return nil
+	return
 }
