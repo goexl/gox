45d539ff0377bc57ac24992db081523f18896939
storezhang <storezhang@gmail.com>
2021-04-08 11:49:25 +0800

    YUNKE-3283 【Server】作为云视课堂运营者，我希望所有学校能在一套系统中运行，而不是每个学校一套部署

--- proxy.go
+++ proxy.go
@@ -5,35 +5,19 @@ import (
 	`net/url`
 )
 
-const (
-	// ProxyTypeSocksV4 Socks代码
-	ProxyTypeSocksV4 ProxyScheme = "socks4"
-	// ProxyTypeSocksV5 Socks代码
-	ProxyTypeSocksV5 ProxyScheme = "socks4"
-	// ProxyTypeSocksV4 Socks代码
-	ProxyTypeHttp ProxyScheme = "http"
-	// ProxyTypeSocksV4 Socks代码
-	ProxyTypeHttps ProxyScheme = "https"
-)
-
-type (
-	// ProxyScheme 代理类型
-	ProxyScheme string
-
-	// Proxy 代理配置
-	Proxy struct {
-		// Host 主机（可以是Ip或者域名）
-		Host string `json:"ip" yaml:"ip" validate:"required"`
-		// Port 端口
-		Port int `default:"80" json:"port" yaml:"port" validate:"required"`
-		// Scheme 代理类型
-		Scheme ProxyScheme `default:"scheme" json:"scheme" yaml:"type" validate:"required,oneof=socks4 socks5 http https"`
-		// Username 代理认证用户名
-		Username string `json:"username" yaml:"username"`
-		// Password 代理认证密码
-		Password string `json:"password" yaml:"password"`
-	}
-)
+// Proxy 代理配置
+type Proxy struct {
+	// Host 主机（可以是Ip或者域名）
+	Host string `json:"ip" yaml:"ip" validate:"required"`
+	// Port 端口
+	Port int `default:"80" json:"port" yaml:"port" validate:"required"`
+	// Scheme 代理类型
+	Scheme URIScheme `default:"scheme" json:"scheme" yaml:"type" validate:"required,oneof=socks4 socks5 http https"`
+	// Username 代理认证用户名
+	Username string `json:"username" yaml:"username"`
+	// Password 代理认证密码
+	Password string `json:"password" yaml:"password"`
+}
 
 func (p *Proxy) Addr() (addr string) {
 	if "" != p.Username && "" != p.Password {
