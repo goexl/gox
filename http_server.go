6faa87154c0b549ea463c0e2d6013c4ddae09461
storezhang <storezhang@gmail.com>
2021-04-09 12:54:43 +0800

    YUNKE-3310 【Web】WEB端跨域，摄像头无法显示

--- http_server.go
+++ http_server.go
@@ -3,8 +3,8 @@ package gox
 // HttpServer Http服务器配置
 type (
 	HttpServer struct {
-		// Ip 监听的地址
-		Ip string `default:"localhost" json:"ip" yaml:"ip" validate:"required"`
+		// Host 监听的地址，可以是IP也可以是域名
+		Host string `default:"localhost" json:"ip" yaml:"ip" validate:"required"`
 		// Port 监听的端口
 		Port int `default:"8081" json:"port" yaml:"port" validate:"required"`
 		// BasePath 前缀地址
