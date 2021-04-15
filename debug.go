d57cbe9076c25c751e09b9ca670fccba6c3e0c2c
storezhang <storezhang@gmail.com>
2021-04-15 17:20:43 +0800

    YUNKE-3310 【Web】WEB端跨域，摄像头无法显示

---
+++ debug.go
@@ -0,0 +1,4 @@
+package gox
+
+// Debug 是否处于调试模式
+type Debug bool
--- elasticsearch.go
+++ elasticsearch.go
@@ -1,5 +1,9 @@
 package gox
 
+import (
+	`net/http`
+)
+
 // ElasticsearchConfig 配置
 type ElasticsearchConfig struct {
 	// Address 地址
@@ -7,5 +11,13 @@ type ElasticsearchConfig struct {
 	// Username 用户名
 	Username string `json:"username" yaml:"username"`
 	// Password 密码
-	Password string `json:"password" yaml:"password"`
+	Password    string `json:"password" yaml:"password"`
+	HealthCheck bool
+	Sniff       bool
+	// Gzip 配置是否启用压缩
+	Gzip bool `default:"false" json:"gzip" yaml:"gzip"`
+	// Headers 传输头
+	Headers http.Header `json:"headers" yaml:"headers"`
+	// Plugins 配置启动时的必备插件
+	Plugins []string `json:"plugins" yaml:"plugins"`
 }
