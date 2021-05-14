cbf66057845876d9b4ffe74cdd22f8b254625d76
storezhang <storezhang@gmail.com>
2021-05-14 10:10:48 +0800

    修复HttpServerConfig中Host字段命名的问题

--- http_server.go
+++ http_server.go
@@ -6,7 +6,7 @@ type (
 		// Host 监听的地址，可以是IP也可以是域名
 		Host string `default:"0.0.0.0" json:"host" yaml:"host" validate:"required"`
 		// Port 监听的端口
-		Port int `default:"8081" json:"port" yaml:"port" validate:"required"`
+		Port int `default:"8080" json:"port" yaml:"port" validate:"required"`
 		// BasePath 前缀地址
 		BasePath string `yaml:"basePath"`
 		// CROS 跨域配置
