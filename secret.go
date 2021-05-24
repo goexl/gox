4809554e0c551c9d46d0e8e90dbe4839929eeb2c
storezhang <storezhang@gmail.com>
2021-05-24 09:58:12 +0800

    给Secret增加缓存Key方法

--- secret.go
+++ secret.go
@@ -1,9 +1,17 @@
 package gox
 
+import (
+	`fmt`
+)
+
 // Secret 描述一个第三方应用授权
 type Secret struct {
 	// 授权，相当于用户名
 	Id string `json:"id" yaml:"id" validate:"required"`
 	// 授权，相当于密码
 	Key string `json:"key" yaml:"key" validate:"required"`
+}
+
+func (s *Secret) CacheKey() string {
+	return fmt.Sprintf("%s-%s", s.Id, s.Key)
 }
