cd58a4bc7baefb5365ff749160cf0870d7c937b6
storezhang <storezhang@gmail.com>
2021-06-16 16:04:39 +0800

    删除不需要的代码

--- certificate.go
+++ certificate.go
@@ -3,19 +3,19 @@ package gox
 type (
 	// ClientCertificate 客户端秘钥
 	ClientCertificate struct {
-		// Public 公钥文件路径
+		// 公钥文件路径
 		Public string `json:"public" yaml:"public" validate:"required,file"`
-		// Private 私钥文件路径
+		// 私钥文件路径
 		Private string `json:"private" yaml:"private" validate:"required,file"`
 	}
 
 	// CertificateConfig 秘钥
 	CertificateConfig struct {
-		// Skip 是否跳过TLS检查
+		// 是否跳过TLS检查
 		Skip bool `default:"true" json:"skip" yaml:"skip"`
-		// Root 根秘钥文件路径
+		// 根秘钥文件路径
 		Root string `json:"root" yaml:"root" validate:"required,file"`
-		// Clients 客户端
+		// 客户端
 		Clients []ClientCertificate `json:"clients" yaml:"clients" validate:"structonly"`
 	}
 )
