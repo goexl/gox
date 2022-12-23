0edb08c02046c2506d85bd7eb412e521e30ee978
storezhang <storezhang@gmail.com>
2022-12-23 09:51:27 +0800

    feat(模型): 移除模型定义

--- deleted_soft.go
+++
@@ -1,7 +0,0 @@
-package gox
-
-// SoftDeleted 带软删除功能的数据库模型
-type SoftDeleted struct {
-	Model   `xorm:"extends"`
-	Deleted `xorm:"extends"`
-}
--- model.go
+++
@@ -1,8 +0,0 @@
-package gox
-
-// Model 基础数据库模型
-type Model struct {
-	Id      `xorm:"extends"`
-	Created `xorm:"extends"`
-	Updated `xorm:"extends"`
-}
