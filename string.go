a7868f84de488f284842a6b3978d457e4df8d7bd
storezhang <storezhang@gmail.com>
2023-02-06 15:03:56 +0800

    feat(字符串): 增加任意类型转换成字符串的方法

---
+++ string.go
@@ -0,0 +1,83 @@
+package gox
+
+import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+)
+
+var _ = Strings
+
+// Strings 将任意类型转换成字符串列表
+func Strings(from ...any) (to []string) {
+	to = make([]string, 0, len(from))
+	for _, value := range from {
+		to = append(to, String(value))
+	}
+
+	return
+}
+
+// String 将任意类型转换为字符串
+func String(from any) (to string) {
+	switch _value := from.(type) {
+	case int8:
+		to = strconv.FormatInt(int64(_value), 10)
+	case *int8:
+		to = strconv.FormatInt(int64(*_value), 10)
+	case uint8:
+		to = strconv.FormatUint(uint64(_value), 10)
+	case *uint8:
+		to = strconv.FormatUint(uint64(*_value), 10)
+	case int:
+		to = strconv.FormatInt(int64(_value), 10)
+	case *int:
+		to = strconv.FormatInt(int64(*_value), 10)
+	case uint:
+		to = strconv.FormatUint(uint64(_value), 10)
+	case *uint:
+		to = strconv.FormatUint(uint64(*_value), 10)
+	case int32:
+		to = strconv.FormatInt(int64(_value), 10)
+	case *int32:
+		to = strconv.FormatInt(int64(*_value), 10)
+	case uint32:
+		to = strconv.FormatUint(uint64(_value), 10)
+	case *uint32:
+		to = strconv.FormatUint(uint64(*_value), 10)
+	case int64:
+		to = strconv.FormatInt(_value, 10)
+	case *int64:
+		to = strconv.FormatInt(*_value, 10)
+	case uint64:
+		to = strconv.FormatUint(_value, 10)
+	case *uint64:
+		to = strconv.FormatUint(*_value, 10)
+	case float32:
+		to = strconv.FormatFloat(float64(_value), 'f', -1, 64)
+	case *float32:
+		to = strconv.FormatFloat(float64(*_value), 'f', -1, 64)
+	case float64:
+		to = strconv.FormatFloat(_value, 'f', -1, 64)
+	case *float64:
+		to = strconv.FormatFloat(*_value, 'f', -1, 64)
+	case bool:
+		to = strconv.FormatBool(_value)
+	case *bool:
+		to = strconv.FormatBool(*_value)
+	case string:
+		to = _value
+	case *string:
+		to = *_value
+	case fmt.Stringer:
+		to = _value.String()
+	case json.Marshaler:
+		if bytes, err := _value.MarshalJSON(); nil == err {
+			to = string(bytes)
+		}
+	default:
+		to = fmt.Sprintf(`%v`, from)
+	}
+
+	return
+}
---
+++ string_test.go
@@ -0,0 +1,29 @@
+package gox_test
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/goexl/gox"
+)
+
+func TestStr(t *testing.T) {
+	toStringTests := []struct {
+		in       []any
+		expected []string
+	}{
+		{[]any{0, 1, 2}, []string{"0", "1", "2"}},
+		{[]any{0.01, 1.01, 2.01}, []string{"0.01", "1.01", "2.01"}},
+		{[]any{"test", 1, 2}, []string{"test", "1", "2"}},
+		{[]any{"test", 1.01, 2.01}, []string{"test", "1.01", "2.01"}},
+		{[]any{"test", 1, 2.01}, []string{"test", "1", "2.01"}},
+		{[]any{"test", "a", "b"}, []string{"test", "a", "b"}},
+	}
+
+	for _, test := range toStringTests {
+		actual := gox.Strings(test.in...)
+		if !reflect.DeepEqual(actual, test.expected) {
+			t.Errorf("str(%v) = %v；期望：%v", test.in, actual, test.expected)
+		}
+	}
+}
