e6cb142d92d05d837a811a4baf39c0e0fd2f9058
storezhang <storezhang@gmail.com>
2023-02-18 21:52:00 +0800

    refactor(字段): 去掉只有指针才能连写的问题

--- fields.go
+++ fields.go
@@ -8,18 +8,18 @@ import (
 // Fields 字段列表
 type Fields[T any] []Field[T]
 
-func (f *Fields[T]) Add(fields ...Field[T]) Fields[T] {
-	final := make([]Field[T], 0, len(*f)+len(fields))
-	final = append(final, *f...)
-	final = append(final, fields...)
-	*f = final
+func (f Fields[T]) Add(fields ...Field[T]) (new Fields[T]) {
+	new = make([]Field[T], 0, len(f)+len(fields))
+	new = append(new, f...)
+	new = append(new, fields...)
+	fields = fields[:0]
 
-	return *f
+	return
 }
 
-func (f *Fields[T]) String() string {
-	kvs := make([]string, 0, len(*f))
-	for _, field := range *f {
+func (f Fields[T]) String() string {
+	kvs := make([]string, 0, len(f))
+	for _, field := range f {
 		kvs = append(kvs, fmt.Sprintf("%s = %v", field.Key(), field.Value()))
 	}
 
