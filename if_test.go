d716f1c57af6f30d2b01cf34772891d7637f9ed8
storezhang <storezhang@gmail.com>
2023-03-01 21:06:39 +0800

    refactor(项目): 去掉不需要的等价代码

--- if_test.go
+++ if_test.go
@@ -7,23 +7,23 @@ import (
 )
 
 func TestIf(t *testing.T) {
-	ift := gox.If[int](true, 8)
+	ift := gox.If(true, 8)
 	if 8 != ift {
 		t.Fatalf("期望：%d，实际：%d", 8, ift)
 	}
 
-	sft := gox.If[string](true, "true")
+	sft := gox.If(true, "true")
 	if "true" != sft {
 		t.Fatalf(`期望：%s，实际：%s`, "true", sft)
 	}
 }
 
 func TestIfx(t *testing.T) {
-	if 8 != gox.Ternary[int](true, 8, 9) {
+	if 8 != gox.Ternary(true, 8, 9) {
 		t.Fatalf(`期望：%d，实际：%d`, 8, 9)
 	}
 
-	if "true" != gox.Ternary[string](true, "true", "false") {
+	if "true" != gox.Ternary(true, "true", "false") {
 		t.Fatalf(`期望：%s，实际：%s`, "true", "false")
 	}
 }
--- set_test.go
+++ set_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestSet(t *testing.T) {
 	_int := 0
-	if gox.Set[int](func(value int) { _int = value }, 8, 9); 8 != _int {
+	if gox.Set(func(value int) { _int = value }, 8, 9); 8 != _int {
 		t.Fatalf(`期望：%d，实际：%d`, 8, 9)
 	}
 }
