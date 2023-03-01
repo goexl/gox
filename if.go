744b180665acb142cf7f113d02d6c3503481bbb0
storezhang <storezhang@gmail.com>
2023-03-01 18:46:58 +0800

    feat(表达式): 增加三元表达式

--- if.go
+++ if.go
@@ -2,13 +2,25 @@ package gox
 
 var (
 	_ = If[int]
+	_ = Iff[int]
 	_ = Ifx[int]
 )
+
+type iff[T any] func() T
 
 // If 模块条件表达式，主要用法是减少大括号
 func If[T any](condition bool, result T) (t T) {
 	if condition {
 		t = result
+	}
+
+	return
+}
+
+// Iff 模块条件表达式，主要用法是减少大括号
+func Iff[T any](condition bool, result iff[T]) (t T) {
+	if condition {
+		t = result()
 	}
 
 	return
@@ -25,6 +37,17 @@ func Ternary[T any](condition bool, first T, second T) (t T) {
 		t = first
 	} else {
 		t = second
+	}
+
+	return
+}
+
+// Ternaryf 模拟三元表达式，主要用法是减少大括号
+func Ternaryf[T any](condition bool, first iff[T], second iff[T]) (t T) {
+	if condition {
+		t = first()
+	} else {
+		t = second()
 	}
 
 	return
