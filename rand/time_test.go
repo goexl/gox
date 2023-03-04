d8e95ddff68a2451b40f70cdbbcfe8613dabc24f
storezhang <storezhang@gmail.com>
2023-03-04 21:14:06 +0800

    test(随机): 增加时间单元测试

---
+++ rand/time_test.go
@@ -0,0 +1,26 @@
+package rand_test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/goexl/gox/rand"
+)
+
+func TestTime(t *testing.T) {
+	tests := []struct {
+		from time.Time
+		to time.Time
+	}{
+		{from: time.Now(), to: time.Now().Add(time.Hour)},
+	}
+
+	for _, test := range tests {
+		got := rand.New().Time().Between(test.from, test.to).Build().Generate()
+		fmt.Println(got)
+		if got.Before(test.from) || got.After(test.to) {
+			t.Fatalf("期望：从%v到%v，实际：%v", test.from, test.to, got)
+		}
+	}
+}
