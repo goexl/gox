a7d13659035567fe525d9cc4330a3c52d90741a8
storezhang <storezhang@gmail.com>
2024-05-15 11:04:25 +0800

    fix(模板): 修复写到文件时，父目录不存在报错的问题

--- tpl/internal/core/base.go
+++ tpl/internal/core/base.go
@@ -2,31 +2,53 @@ package core
 
 import (
 	"os"
+	"path/filepath"
 )
 
 type Base struct {
 	// 空结构
 }
 
-func (b *Base) RenderToFile(filepath string, fun Function) (err error) {
-	var file *os.File
-	if _, se := os.Stat(filepath); nil != se && os.IsNotExist(se) {
-		file, err = os.Create(filepath)
+func (b *Base) RenderToFile(path string, fun Function) (err error) {
+	if file, gfe := b.getFile(path); nil != gfe {
+		b.close(file)
+		err = gfe
 	} else {
-		file, err = os.Open(filepath)
-	}
-	if nil != err {
-		return
+		err = b.render(file, fun)
 	}
 
-	defer b.Close(file, &err)
+	return
+}
+
+func (b *Base) render(file *os.File, fun Function) (err error) {
+	defer b.close(file)
 	err = fun(file)
 
 	return
 }
 
-func (b *Base) Close(file *os.File, err *error) {
-	if ce := file.Close(); nil != ce {
-		*err = ce
+func (b *Base) getFile(path string) (file *os.File, err error) {
+	if _, se := os.Stat(path); nil != se && os.IsNotExist(se) {
+		file, err = b.create(path)
+	} else {
+		file, err = os.OpenFile(path, os.O_WRONLY, os.ModePerm)
 	}
+
+	return
+}
+
+func (b *Base) create(path string) (file *os.File, err error) {
+	dir := filepath.Dir(path)
+	if _, se := os.Stat(dir); nil != se && os.IsNotExist(se) {
+		err = os.MkdirAll(dir, os.ModePerm)
+	}
+	if nil == err {
+		file, err = os.Create(path)
+	}
+
+	return
+}
+
+func (b *Base) close(file *os.File) {
+	_ = file.Close()
 }
