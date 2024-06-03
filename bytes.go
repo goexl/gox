1eab5d70c86aad254ec19372b1dc81ce1340dff6
storezhang <storezhang@gmail.com>
2024-06-03 15:14:23 +0800

    feat(大小): 暴露常量

--- bytes.go
+++ bytes.go
@@ -4,7 +4,20 @@ import (
 	"github.com/goexl/gox/internal/size"
 )
 
-var _ = ParseBytes
+const (
+	BytesB  = size.BytesB
+	BytesKB = 1024 * BytesB
+	BytesMB = 1024 * BytesKB
+	BytesGB = 1024 * BytesMB
+	BytesTB = 1024 * BytesGB
+	BytesPB = 1024 * BytesTB
+	BytesEB = 1024 * BytesPB
+)
+
+var (
+	_ = ParseBytes
+	_ = BytesEB
+)
 
 // Bytes 字节大小
 type Bytes = size.Bytes
