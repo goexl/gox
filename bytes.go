00c588766547ab675e6e5b56bcdf6df131baa246
storezhang <storezhang@gmail.com>
2024-06-03 11:47:33 +0800

    feat(大小): 暴露ParseBytes

--- bytes.go
+++ bytes.go
@@ -4,5 +4,12 @@ import (
 	"github.com/goexl/gox/internal/size"
 )
 
+var _ = ParseBytes
+
 // Bytes 字节大小
 type Bytes = size.Bytes
+
+// ParseBytes 解析字节大小
+func ParseBytes(from string) (Bytes, error) {
+	return size.ParseBytes(from)
+}
