c3e4ec547c793ff2ff3dbe18206fd3a1ce1b5e34
storezhang <storezhang@gmail.com>
2024-08-28 12:01:55 +0800

    feat(切片): 增加切片功能

---
+++ array.go
@@ -0,0 +1,45 @@
+package gox
+
+var (
+	_ = Index[int]
+	_ = Contains[int]
+	_ = Diff[int]
+)
+
+// Index 在列表中找出元素的下标
+func Index[T any](items *[]T, item T) (index int) {
+	index = -1
+	for _index, _value := range *items {
+		if any(item) == any(_value) {
+			index = _index
+		}
+		if -1 != index {
+			break
+		}
+	}
+
+	return
+}
+
+// Contains 判断元素是否在数组中
+func Contains[T any](items *[]T, item T) bool {
+	return -1 != Index(items, item)
+}
+
+// Diff 差集
+func Diff[T any](first *[]T, second *[]T) (diff []T) {
+	set := make(map[any]bool)
+	diff = make([]T, 0)
+
+	for _, value := range *first {
+		set[value] = true
+	}
+	for _, value := range *second {
+		if !set[value] {
+			diff = append(diff, value)
+		}
+	}
+	set = nil
+
+	return
+}
--- slice.go
+++ slice.go
@@ -1,45 +1,31 @@
 package gox
 
-var (
-	_ = Index[int]
-	_ = Contains[int]
-	_ = Diff[int]
+import (
+	"encoding/json"
+	"encoding/xml"
 )
 
-// Index 在列表中找出元素的下标
-func Index[T any](items *[]T, item T) (index int) {
-	index = -1
-	for _index, _value := range *items {
-		if any(item) == any(_value) {
-			index = _index
-		}
-		if -1 != index {
-			break
-		}
+// Slice 切片，既可以兼容单个值也可以兼容数组
+type Slice[T any] []*T
+
+func (s *Slice[T]) UnmarshalJSON(bytes []byte) (err error) {
+	t := new(T)
+	if ue := json.Unmarshal(bytes, t); nil != ue {
+		err = json.Unmarshal(bytes, s)
+	} else {
+		*s = []*T{t}
 	}
 
 	return
 }
 
-// Contains 判断元素是否在数组中
-func Contains[T any](items *[]T, item T) bool {
-	return -1 != Index(items, item)
-}
-
-// Diff 差集
-func Diff[T any](first *[]T, second *[]T) (diff []T) {
-	set := make(map[any]bool)
-	diff = make([]T, 0)
-
-	for _, value := range *first {
-		set[value] = true
+func (s *Slice[T]) UnmarshalXML(decoder *xml.Decoder, start xml.StartElement) (err error) {
+	t := new(T)
+	if ue := decoder.DecodeElement(t, &start); nil != ue {
+		err = decoder.DecodeElement(s, &start)
+	} else {
+		*s = []*T{t}
 	}
-	for _, value := range *second {
-		if !set[value] {
-			diff = append(diff, value)
-		}
-	}
-	set = nil
 
 	return
 }
