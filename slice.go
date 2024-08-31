cb87d9dabcb12ceca51c77601346b51403c300a0
storezhang <storezhang@gmail.com>
2024-09-01 07:41:40 +0800

    feat(切片): 增加Clone方法

--- slice.go
+++ slice.go
@@ -6,14 +6,21 @@ import (
 )
 
 // Slice 切片，既可以兼容单个值也可以兼容数组
-type Slice[T any] []*T
+type Slice[T any] []T
+
+func (s *Slice[T]) Clone() (t Slice[T]) {
+	t = make(Slice[T], len(*s))
+	copy(t, *s)
+
+	return
+}
 
 func (s *Slice[T]) UnmarshalJSON(bytes []byte) (err error) {
 	t := new(T)
 	if ue := json.Unmarshal(bytes, t); nil != ue {
 		err = json.Unmarshal(bytes, s)
 	} else {
-		*s = []*T{t}
+		*s = []T{*t}
 	}
 
 	return
@@ -24,7 +31,7 @@ func (s *Slice[T]) UnmarshalXML(decoder *xml.Decoder, start xml.StartElement) (e
 	if ue := decoder.DecodeElement(t, &start); nil != ue {
 		err = decoder.DecodeElement(s, &start)
 	} else {
-		*s = []*T{t}
+		*s = []T{*t}
 	}
 
 	return
